f52f2ea0dcf113e04e3800e8d59c4c25ea508a13
Christian Braccio <christianbraccio@gmail.com>
2025-11-21 15:56:50 +0100

    Adding a loop to the method SortBooksBy

--- Library/Utilities/Bookshelf.cs
+++ Library/Utilities/Bookshelf.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 using static System.Reflection.Metadata.BlobBuilder;
 
 namespace Library.Utilities
@@ -20,108 +21,120 @@ namespace Library.Utilities
 
         public void SortBooksBy(List<Book> books)
         {
-            Console.WriteLine("Choose a sort option:");
-            Console.WriteLine("1) Sort by title ");
-            Console.WriteLine("2) Sort by author ");
-            Console.WriteLine("3) Sort by rating ");
-            Console.WriteLine("4) Sort by genre ");
-            int choice = int.Parse(Console.ReadLine());
-
-            switch (choice)
+            int choice = default;
+            do
             {
-                case 1:
-                    var titleSortedBooks = books
-                                      .OrderBy(b => b.Title)
-                                      .ToList();
+                Console.WriteLine("Choose a sort option:");
+                Console.WriteLine("0) Exit ");
+                Console.WriteLine("1) Sort by title ");
+                Console.WriteLine("2) Sort by author ");
+                Console.WriteLine("3) Sort by rating ");
+                Console.WriteLine("4) Sort by genre ");
+                choice = int.Parse(Console.ReadLine());
 
-                    Console.WriteLine("Write a title to search... ");
-                    string customTitleToSearch = Console.ReadLine();
+                switch (choice)
+                {
+                    case 1:
+                        var titleSortedBooks = books
+                                          .OrderBy(b => b.Title)
+                                          .ToList();
 
-                    var customTitleSearch = books.Where(b => b.Title.Equals(customTitleToSearch, StringComparison.OrdinalIgnoreCase)).ToList();
+                        Console.WriteLine("Write a title to search... ");
+                        string customTitleToSearch = Console.ReadLine();
 
-                    if (customTitleSearch.Count == 0) { Console.WriteLine("Sorry, this title is currently unavailable"); }
-                    else 
-                    {
-                        foreach (var book in customTitleSearch)
+                        var customTitleSearch = books.Where(b => b.Title.Equals(customTitleToSearch, StringComparison.OrdinalIgnoreCase)).ToList();
+
+                        if (customTitleSearch.Count == 0) { Console.WriteLine("Sorry, this title is currently unavailable"); }
+                        else
                         {
-                            Console.WriteLine($"\nBook sorted:\nTitle: {book.Title}\nAuthor: {book.Author}\nGenre: {book.Genre}\nLore: {book.Lore}\nRating: {book.Rating}/5");
+                            foreach (var book in customTitleSearch)
+                            {
+                                Console.WriteLine($"\nBook sorted:\nTitle: {book.Title}\nAuthor: {book.Author}\nGenre: {book.Genre}\nLore: {book.Lore}\nRating: {book.Rating}/5");
+                            }
                         }
-                    }
 
-                    break;
+                        break;
 
-                case 2:
-                    var authorSortedBooks = books
-                                      .OrderBy(b => b.Author)
-                                      .ToList();
+                    case 2:
+                        var authorSortedBooks = books
+                                          .OrderBy(b => b.Author)
+                                          .ToList();
 
-                    Console.WriteLine("Write a title to search... ");
-                    string customAuthorToSearch = Console.ReadLine();
+                        Console.WriteLine("Write a title to search... ");
+                        string customAuthorToSearch = Console.ReadLine();
 
-                    var customAuthorSearch = books.Where(b => b.Author.Equals(customAuthorToSearch, StringComparison.OrdinalIgnoreCase)).ToList();
+                        var customAuthorSearch = books.Where(b => b.Author.Equals(customAuthorToSearch, StringComparison.OrdinalIgnoreCase)).ToList();
 
-                    if (customAuthorSearch.Count == 0) { Console.WriteLine("Sorry, this title is currently unavailable"); }
-                    else
-                    {
-                        foreach (var book in customAuthorSearch)
+                        if (customAuthorSearch.Count == 0) { Console.WriteLine("Sorry, this title is currently unavailable"); }
+                        else
                         {
-                            Console.WriteLine($"\nBook sorted:\nTitle: {book.Title}\nAuthor: {book.Author}\nGenre: {book.Genre}\nLore: {book.Lore}\nRating: {book.Rating}/5");
+                            foreach (var book in customAuthorSearch)
+                            {
+                                Console.WriteLine($"\nBook sorted:\nTitle: {book.Title}\nAuthor: {book.Author}\nGenre: {book.Genre}\nLore: {book.Lore}\nRating: {book.Rating}/5");
+                            }
                         }
-                    }
 
-                    break;
+                        break;
 
-                case 3:
-                    var ratingSortedBooks = books
-                                      .OrderBy(b => b.Rating)
-                                      .ToList();
+                    case 3:
+                        var ratingSortedBooks = books
+                                          .OrderBy(b => b.Rating)
+                                          .ToList();
 
-                    Console.WriteLine("Write a rating to search... ");
-                    int customRatingToSearch = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Write a rating to search... ");
+                        int customRatingToSearch = int.Parse(Console.ReadLine());
 
-                    var customRatingSearch = books.OrderBy(b => b.Rating.Equals(customRatingToSearch).ToString());
+                        var customRatingSearch = books.OrderBy(b => b.Rating.Equals(customRatingToSearch).ToString());
 
                         foreach (var book in customRatingSearch)
                         {
                             Console.WriteLine($"\nBook sorted:\nTitle: {book.Title}\nAuthor: {book.Author}\nGenre: {book.Genre}\nLore: {book.Lore}\nRating: {book.Rating}/5");
                         }
 
-                    break;
+                        break;
 
-                case 4:
-                    var genreSortedBooks = books
-                                      .OrderBy(b => b.Genre)
-                                      .ToList();
+                    case 4:
+                        var genreSortedBooks = books
+                                          .OrderBy(b => b.Genre)
+                                          .ToList();
 
-                    Console.WriteLine("Write a genre to search... ");
-                    Console.WriteLine("1) Action ");
-                    Console.WriteLine("2) Adventure ");
-                    Console.WriteLine("3) Comedy ");
-                    Console.WriteLine("4) Drama ");
-                    Console.WriteLine("5) Fantasy ");
-                    Console.WriteLine("6) Horror ");
-                    Console.WriteLine("7) Mystery ");
-                    Console.WriteLine("8) Romance ");
-                    Console.WriteLine("9) SciFi ");
-                    int customGenreToSearch = int.Parse(Console.ReadLine());
-                    //string myEnumValue = Enum.GetName(typeof (Genres), customGenreToSearch);
+                        Console.WriteLine("Write a genre to search... ");
+                        Console.WriteLine("1) Action ");
+                        Console.WriteLine("2) Adventure ");
+                        Console.WriteLine("3) Comedy ");
+                        Console.WriteLine("4) Drama ");
+                        Console.WriteLine("5) Fantasy ");
+                        Console.WriteLine("6) Horror ");
+                        Console.WriteLine("7) Mystery ");
+                        Console.WriteLine("8) Romance ");
+                        Console.WriteLine("9) SciFi ");
+                        int customGenreToSearch = int.Parse(Console.ReadLine());
+                        //string myEnumValue = Enum.GetName(typeof (Genres), customGenreToSearch);
 
-                    var customGenreSearch = books.OrderBy(b => b.Genre = (Genres)customGenreToSearch).ToList();
-                    if (customGenreSearch.Count == 0) { Console.WriteLine("Sorry, this title is currently unavailable"); }
-                    else
-                    {
-                        foreach (var book in books)
+                        var customGenreSearch = books.OrderBy(b => b.Genre = (Genres)customGenreToSearch).ToList();
+                        if (customGenreSearch.Count == 0) { Console.WriteLine("Sorry, this title is currently unavailable"); }
+                        else
                         {
-                            Console.WriteLine($"\nBook sorted:\nTitle: {book.Title}\nAuthor: {book.Author}\nGenre: {book.Genre}\nLore: {book.Lore}\nRating: {book.Rating}/5");
+                            foreach (var book in books)
+                            {
+                                Console.WriteLine($"\nBook sorted:\nTitle: {book.Title}\nAuthor: {book.Author}\nGenre: {book.Genre}\nLore: {book.Lore}\nRating: {book.Rating}/5");
+                            }
                         }
-                    }
 
-                    break;
+                        break;
 
-                default:
-                    Console.WriteLine("Choose a number between 1 and 4 please");
-                    break;
-            }
+                    default:
+                        if (choice >= 1 || choice <= 4)
+                        {
+                            Console.WriteLine("Choose a number between 1 and 4 please");
+                        }
+                        else if(choice == 0)
+                        {
+                            Console.WriteLine("Leaving sorting menu...");
+                        }
+                        break;
+                }
+            } while (choice != 0);
         }
     }
 }
